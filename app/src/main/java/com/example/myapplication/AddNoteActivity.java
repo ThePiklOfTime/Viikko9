f173392e899565cc0c60716b2589173db21cb108
ThePiklOfTime <nicotolvanen@gmail.com>
2026-03-17 00:00:42 +0200

    Added notes to notestorage and fixed it being in a wrong file. Oops

--- app/src/main/java/com/example/myapplication/AddNoteActivity.java
+++ app/src/main/java/com/example/myapplication/AddNoteActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddNoteActivity extends AppCompatActivity {
+    EditText title;
+    EditText content;
+
+    NoteStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_note);
+
+        title = findViewById(R.id.TitleEdit);
+        content = findViewById(R.id.ContentEdit);
+        storage = NoteStorage.getInstance();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void addNote() {
+        String tt = title.toString();
+        String cc = content.toString();
+        Note note = new Note(tt, cc);
+        storage.addNote(note);
+
     }
 }
