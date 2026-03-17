333cc98117cdd8e4596a02d67bec3bbfa9cb1cd7
ThePiklOfTime <nicotolvanen@gmail.com>
2026-03-17 03:37:46 +0200

    Added all functionality

--- app/src/main/java/com/example/myapplication/Note.java
+++ app/src/main/java/com/example/myapplication/Note.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import java.util.Calendar;
+
 public class Note {
 
-    int Id;
+    static int Id = 0;
     String title;
     String content;
     String timeAndDate;
@@ -10,6 +12,20 @@ public class Note {
     public Note(String title, String content){
         this.title = title;
         this.content = content;
+        Id++;
+        Calendar cal = Calendar.getInstance();
+        String hr = String.valueOf(cal.get(Calendar.HOUR_OF_DAY));
+        String mn = String.valueOf(cal.get(Calendar.MINUTE));
+        String Day = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
+        String MM = String.valueOf(cal.get(Calendar.MONTH));
+        String yy = String.valueOf(cal.get(Calendar.YEAR));
+        timeAndDate = hr + ": " + mn + " " + Day + "." + MM + "." + yy;
+
+
+
     }
 
+    public static int getId() {
+        return Id;
+    }
 }
